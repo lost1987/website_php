5e37a91299b8d4b374122c00e946e926ac78a600
峘 卜 <535051216@qq.com>
2015-11-16 20:27:37 +0800

    项目迁移至私人

--- inner/adm/assets/standalone/children.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by shameless on 15/6/16.
- */
-var action='/user/children',data='',args=new Object();
-function doSearch(){
-    args.url = action;
-    args.data = "uid="+uid;
-    return args;
-}
--- platform/pms/media/js/common/service_reply.js
+++
@@ -1,9 +0,0 @@
-(function($){
-        $("input[name='reply_radio']").on('click',function(){
-                    if($(this).val() == '自定义'){
-                        $("#custom_reply").attr('disabled',false);
-                    }else{
-                        $("#custom_reply").attr('disabled',true);
-                    }
-        });
-})(jQuery)
